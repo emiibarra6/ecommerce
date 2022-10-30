e8055d7ef75dca9800980f51309e3bd965669dcf
emiibarra6 <emiibarraa@hotmail.com>
2022-10-30 01:28:01 -0300

    3 rutas protegidas por jwt

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { actualizarProducto, borrarProducto, guardarProducto , obtenerProductoPorID, traeTodosLosProductos} from "../controller/productos.controller.js";
+import checkAuth from "../middleware/authMiddleware.js";
 
 const router = Router();
 router.get('/' , traeTodosLosProductos)
-router.post('/' , guardarProducto)
+router.post('/' , checkAuth ,guardarProducto)
 router.get('/:id' , obtenerProductoPorID)
-router.patch('/:id' , actualizarProducto)
-router.delete('/:id' , borrarProducto)
+router.patch('/:id' , checkAuth , actualizarProducto)
+router.delete('/:id' , checkAuth ,borrarProducto)
 
 export default router;
