a7bc0f1de9abcb4a1049e9b77783a9fd54368833
emiibarra6 <emiibarraa@hotmail.com>
2022-10-30 23:02:40 -0300

    rutas para usuarios

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import routerProductos from './productos.routes.js'
+import routerUsuario from './usuario.routes.js'
 
 function routerApi(app){
     const router = Router();
     app.use('/api/v1' , router)
     
     router.use('/productos' , routerProductos)
+    router.use('/usuario' , routerUsuario)
 }
 
 
---
+++ src/routes/usuario.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { crearUsuario, obtenerTodosLosUsuarios, autenticarUsuario} from "../controller/usuario.controller";
+import checkAuth from "../middleware/authMiddleware.js";
+
+const router = Router();
+router.get('/' , obtenerTodosLosUsuarios)
+router.post('/' , checkAuth ,crearUsuario)
+router.post('/auth' , autenticarUsuario)
+
+
+export default router;
