85ea04d30ef79a1197f617020d402c4c797ea5d3
Emi <emiibarraa@hotmail.com>
2022-11-15 23:45:25 -0300

    relacion productos categoria

--- src/models/index.model.js
+++ src/models/index.model.js
@@ -1,10 +1,13 @@
 import { ventasDetalledb } from './ventas.detalle.model.js'
 import { ventasdb } from './ventas.model.js'
 import { productosdb } from './productos.models.js'
+import { categoriasdb } from './categoria.models.js'
 
 //relaciones
 ventasdb.hasMany(ventasDetalledb, {foreignKey: 'id_venta'}),
+
 ventasDetalledb.belongsTo(ventasdb, {foreignKey: 'id_venta'}),
 ventasDetalledb.belongsTo(productosdb, {foreignKey: 'id_producto'})
 
+productosdb.belongsTo( categoriasdb, {foreignKey: 'id_categoria'} )
 export { ventasDetalledb, ventasdb }
